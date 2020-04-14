f8359007d616b42f55279bc95a6db2dd5c385f86
Song MyungHoon <smh90510@gmail.com>
2020-04-14 10:40:48 +0900

    Day5 morning Java class Lecture update

---
+++ src/Day5/PracProb6_1.java
@@ -0,0 +1,11 @@
+package Day5;
+
+public class PracProb6_1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("3초간 기다림!");
+
+        // 3초간 기다림
+
+        System.out.println("끝");
+    }
+}
