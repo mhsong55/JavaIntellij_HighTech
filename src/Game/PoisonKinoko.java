ba367c2948755930720e073676753849436ff02b
Song MyungHoon <smh90510@gmail.com>
2020-04-22 11:22:51 +0900

    Day10. applied review of Practice
    revised Practice problem of Chapter 10-3.

--- src/Game/PoisonKinoko.java
+++ src/Game/PoisonKinoko.java
@@ -1,10 +1,11 @@
 package Game;
 
 public class PoisonKinoko extends Kinoko {
-    private int poisonAttackCount = 5;
+    private int poisonAttackCount;
 
     public PoisonKinoko(char suffix) {
         super(suffix);
+        poisonAttackCount = 5;
     }
 
     public int getPoisonAttackCount() {
@@ -16,9 +17,9 @@ public class PoisonKinoko extends Kinoko {
         super.attack(hero);
         if (poisonAttackCount > 0) {
             System.out.println("추가로, 독 포자를 살포했다!");
-            int poisonAttack = (int) (hero.getHp() / 5);
-            hero.setHp(hero.getHp() - poisonAttack);
-            System.out.println(poisonAttack + "포인트의 데미지");
+            int poisonDamage = hero.getHp() / 5;
+            hero.setHp(hero.getHp() - poisonDamage);
+            System.out.println(poisonDamage + "포인트의 데미지");
             poisonAttackCount--;
         }
     }
